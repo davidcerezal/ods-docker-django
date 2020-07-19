a87391c7780d37fefe8aafa427e35539a8f438bf
davidcerezal <david.cerezal77@gmail.com>
2020-07-19 10:53:04 +0200

    Combined Surebets model

--- src/odds/domain/models/factories/sureBet.py
+++ src/odds/domain/models/factories/sureBet.py
@@ -7,10 +7,21 @@ class SureBetFactory(object):
 
     @classmethod
     def create(cls, name, benefit, bets, event, revised=False):
-        surebet = SureBet(name=name, benefit=benefit, revised=revised)
+        surebet = SureBet(name=name, benefit=benefit, revised=revised, type=SureBet.TYPE_NORMAL)
         surebet.save()
         surebet.events.add(event)
         for bet in bets:
             surebet.bets.add(bet)
+
+        return surebet
+
+
+    @classmethod
+    def createCombined(cls, name, benefit, bets, combinedBets, event, revised=False):
+        surebet = SureBetFactory.create(name=name, benefit=benefit, bets=bets, event=event, revised=revised)
+        surebet.type = SureBet.TYPE_COMBINED
+        for bet in combinedBets:
+            surebet.combined_bets_first.add(bet)
+        surebet.save()
 
         return surebet
--- src/odds/domain/models/sureBet.py
+++ src/odds/domain/models/sureBet.py
@@ -3,15 +3,21 @@ from .bet import Bet
 from .event import Event
 import uuid
 
-
 class SureBet(models.Model):
+
+    TYPE_NORMAL = 'normal'
+    TYPE_COMBINED = 'combined'
+
     name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, blank=True)
     uuid = models.CharField(max_length=255,  blank=True, unique=True, default=uuid.uuid4)
     value = models.CharField(max_length=20, blank=True)
     benefit = models.FloatField(max_length=20, blank=True)
     bets = models.ManyToManyField(Bet, blank=True)
     events = models.ManyToManyField(Event, blank=True)
     revised = models.BooleanField(default=False)
+    combined_bets_first = models.ManyToManyField(Bet, blank=True, related_name='firs_combined')
+    combined_bets_second = models.ManyToManyField(Bet, blank=True, related_name='second_combined')
 
     def __str__(self):
         return self.name
